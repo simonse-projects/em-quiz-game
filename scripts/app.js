75f6e58a2cb29ab32f8306494793065b16e54320
Courtney Simonse <courtney.r.elder@gmail.com>
2019-11-23 17:50:05 -0500

    save dropdown selection

--- scripts/app.js
+++ scripts/app.js
@@ -10,16 +10,43 @@
     console.log("Error loading data");
   });
 
-
   function displayCases(cases) {
 
+    // empty case list
     $("#case-selection").empty();
+    // loop through cases to build list
     $.each(cases, function (index, value) {
-      $("#case-selection").append("<option>"+
+      $("#case-selection").append("<option value="+value["CaseNumber"]+">"+
           value["PatientName"]+" - "+value["ChiefComplaint"]+
           "</option>");
     });
 
-  }
+    // button selects case
+    $("#select-btn").on("click", function (e) {
+      console.log($("#case-selection").children("option:selected").val());
+      $("#case-list").modal("hide");
+
+      // select case with selected ID number
+      var caseNumber = $("#case-selection").children("option:selected").val();
+      var caseDetails = [];
+      $.each(cases, function (index, value) {
+        if (value["CaseNumber"] == caseNumber) {
+          caseDetails = value;
+        }
+      });
+
+      // hide case selection area and show case details
+      $("#page-one").hide();
+      $("#page-three").show();
+
+      // build case details page
+      showCase(caseDetails);
+    });
+
+  } //end displayCases
+
+function showCase(details) {
+  $("#patient-name").append(details["PatientName"])
+}
 
 })();
