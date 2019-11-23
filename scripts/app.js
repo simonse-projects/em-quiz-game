5531d0ceb5cfcea3ef0feb66d78aeccc13320fc0
Courtney Simonse <courtney.r.elder@gmail.com>
2019-11-23 15:32:00 -0500

    add .gitignore

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,20 @@
-$("#page-three").hide();
-// $("#page-one").hide();
+(function () {
+  // hide patient info screen to start
+  $("#page-three").hide();
+  // $("#page-one").hide();
+
+  Papa.parse("data/CaseDatawHeaders.csv", {
+    header: true,
+    delimiter: ",",
+    complete: function(results) {
+      console.log("Finished");
+      displayCases(results);
+    }
+  })
+
+
+  function displayCases(cases) {
+    console.log(cases);
+  }
+
+})();
